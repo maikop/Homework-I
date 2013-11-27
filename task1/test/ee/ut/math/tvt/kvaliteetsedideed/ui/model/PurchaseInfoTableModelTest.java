1b55058a4420139cce4954e93a579336c7199fa4
maikop <maikop@ut.ee>
2013-11-27 15:17:59 +0200

    PurchaseInfoTableModelTest

--- task1/test/ee/ut/math/tvt/kvaliteetsedideed/ui/model/PurchaseInfoTableModelTest.java
+++ task1/test/ee/ut/math/tvt/kvaliteetsedideed/ui/model/PurchaseInfoTableModelTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import ee.ut.math.tvt.kvaliteetsedideed.domain.data.SoldItem;
 import ee.ut.math.tvt.kvaliteetsedideed.domain.data.StockItem;
+import ee.ut.math.tvt.kvaliteetsedideed.domain.exception.VerificationFailedException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,11 +17,13 @@ public class PurchaseInfoTableModelTest {
   private StockItem si;
 
   @Before
-  public void setup() {
+  public void setup() throws VerificationFailedException {
     pit = new PurchaseInfoTableModel();
-    si = new StockItem((long) 17, "test", "test", 1.0, 1);
+    si = new StockItem((long) 170, "test", "test", 1.0, 2);
     s = new SoldItem(si, 1);
-    pit.addItem(s);
+    List<SoldItem> list = new ArrayList<SoldItem>();
+    list.add(s);
+    pit.populateWithData(list);
 
   }
 
@@ -27,12 +32,13 @@ public class PurchaseInfoTableModelTest {
     assertEquals(pit.getColumnValue(s, 0), si.getId());
     assertEquals(pit.getColumnValue(s, 1), si.getName());
     assertEquals(pit.getColumnValue(s, 2), si.getPrice());
-    assertEquals(pit.getColumnValue(s, 3), si.getQuantity());
+    assertEquals(pit.getColumnValue(s, 3), s.getQuantity());
   }
 
   @Test
   public void testGetItemByStockItemId() {
 
-    assertEquals(pit.getItemByStockItemId((long) 17), s);
+    assertEquals(pit.getItemByStockItemId((long) 170), s);
+
   }
 }
