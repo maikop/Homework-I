d30190649599d11edd9235c2cea6d4cf6a6f6e80
Aleksei Panarin <aleksei.panarin@gmail.com>
2013-11-27 15:23:42 +0200

    StockTableModelTest: 3 tests (of 4)

---
+++ task1/test/ee/ut/math/tvt/kvaliteetsedideed/domain/data/StockTableModelTest.java
@@ -0,0 +1,44 @@
+package ee.ut.math.tvt.kvaliteetsedideed.domain.data;
+
+import ee.ut.math.tvt.kvaliteetsedideed.ui.model.StockTableModel;
+import java.util.NoSuchElementException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StockTableModelTest {
+
+  StockTableModel model;
+  StockItem item1;
+
+  @Before
+  public void setUp() {
+    model = new StockTableModel();
+    item1 = new StockItem(1L, "test1", "descr1", 10.0, 5);
+    model.addItem(item1);
+    model.addItem(new StockItem(2L, "test2", "descr2", 50.0, 3));
+
+  }
+
+  @Test
+  public void testValidateNameUniqueness() {
+
+  }
+
+  @Test
+  public void testHasEnoughInStock() {
+    Assert.assertTrue(model.hasEnoughInStock(item1, 5));
+    Assert.assertFalse(model.hasEnoughInStock(item1, 6));
+  }
+
+  @Test
+  public void testGetItemByIdWhenItemExists() {
+    Assert.assertEquals(item1, model.getItemById(1L));
+  }
+
+  @Test(expected = NoSuchElementException.class)
+  public void testGetItemByIdWhenThrowsException() {
+    Assert.assertEquals(item1, model.getItemById(3L));
+  }
+
+}
